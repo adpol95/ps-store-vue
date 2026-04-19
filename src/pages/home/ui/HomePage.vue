553e5abf90b832d79006f3186e6b928ab58dac56
adpol95 <adpol95@gmail.com>
2026-04-19 12:52:45 +0300

    fix: type check ci

--- src/pages/home/ui/HomePage.vue
+++ src/pages/home/ui/HomePage.vue
@@ -5,7 +5,7 @@ import { AddToCart } from "@/features/add-to-cart";
 
 <template>
     <ProductCard
-        genre="Action"
+        :genre="23"
         image="https://image.api.playstation.com/vulcan/ap/rnd/202512/1205/79661d7a2bdb9784749b4e57e1456ca89f7ac7bed8615aee.png?w=620&thumb=false"
         title="Resident Evil 9"
         :price="34"
